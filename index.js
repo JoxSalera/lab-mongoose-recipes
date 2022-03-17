1d34b773b17ef5be4257e0f2778bfe24fbab815c
JoxSalera <97227244+JoxSalera@users.noreply.github.com>
2022-03-17 18:33:10 +0100

    Completed lab

--- index.js
+++ index.js
@@ -1,23 +1,62 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
-  .then(x => {
+  .then((x) => {
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
+    return Recipe.deleteMany();
+  })
+
+  // recipe created in lab-iteration 2
+  .then(() => {
+    const cake = Recipe.create({
+      title: "Cake",
+      level: "Amateur Chef",
+      ingredients: [
+        "6 cups grated carrots",
+        "1 cup brown sugar",
+        "1 cup raisins",
+        "4 eggs",
+        "1 1/2 cups white sugar",
+        "1 cup vegetable oil",
+        "2 teaspoons vanilla extract",
+        "1 cup crushed pineapple, drained",
+        "3 cups all-purpose flour",
+        "1 1/2 teaspoons baking soda",
+        "1 teaspoon salt",
+        "4 teaspoons ground cinnamon",
+      ],
+      cuisine: "International",
+      dishType: "dessert",
+      duration: 130,
+      creator: "Chef ALex",
+    });
+    console.log(cake);
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    return Recipe.insertMany(data);
   })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+  .then((data) => {
+    console.log(data);
+    return Recipe.findOneAndUpdate(
+      { title: "Rigatoni alla Genovese" },
+      { duration: 100 }
+    );
+  })
+  .then(() => {
+    console.log("Carrot Cake successfully removed");
+    return Recipe.deleteOne({ title: "Carrot Cake" });
+  })
+  .catch((error) => {
+    console.error("Error connecting to the database", error);
   });
+mongoose.connection.close();
